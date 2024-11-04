623c1c634a442963e3fb57880ce704c7d8a2a1c3
Sammi Ghazzawi <swghazzawi@gmail.com>
2024-11-03 20:41:26 -0700

    Update Header.tsx againmmm

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Link as ScrollLink } from 'react-scroll';
 
 const buttonStyle = {
